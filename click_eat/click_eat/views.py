c72e52ebff9013d341c4e2d61442d64413ae8bd3
kaplonpaulina <Koliber123>
2018-12-18 21:49:30 +0100

    replacing view classes with functions

--- click_eat/click_eat/views.py
+++ click_eat/click_eat/views.py
@@ -2,11 +2,6 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render
 
-class TestPage(TemplateView):
-    template_name = 'test.html'
-
-class ThanksPage(TemplateView):
-    template_name = 'thanks.html'
-
-class HomePage(TemplateView):
-    template_name = 'index.html'
+def home(request):
+    template = 'index.html'
+    return render(request,template)
